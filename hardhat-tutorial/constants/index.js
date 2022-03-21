419164d1766a8edc2efd86596f8d8050ecafa20c
Niall Stapleton <niall.stapleton@gmail.com>
2022-03-21 18:09:51 +0000

    inital my-app

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xB054427c83f58Dd637aB2a2883FFaE316FBbFdB4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
